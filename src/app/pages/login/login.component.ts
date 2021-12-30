ca661662cba1dd8c9f12444d01392ea6e52e91ae
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2021-12-30 16:10:25 +0300

    feat: login component add

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user-info';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+  model: User = new User();
+
+  ngOnInit(): void {
+  }
+  login(form:NgForm){
+    alert(this.model.password+this.model.userName)
+  }
+
+}
