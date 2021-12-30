eab643db836e2145bd6b3cb5db96af93b30c2aea
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2021-12-30 16:09:12 +0300

    refactor:footer revised

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,4 +1,9 @@
+import { from } from 'rxjs';
+
 export * from './homepage/homepage.component';
 export * from './gallery/gallery.component';
 export * from './about-us/about-us.component';
 export * from './contact-us/contact-us.component';
+export * from './login/login.component';
+export * from './sign-up/sign-up.component';
+export * from './comment/comment.component';
