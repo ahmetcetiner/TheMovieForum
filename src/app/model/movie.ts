20e7cf4dd53f80fe1fc5f36dce35d1d448f49731
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-31 17:25:55 +0300

    feat: Movie.ts added.

---
+++ src/app/model/movie.ts
@@ -0,0 +1,12 @@
+export class Movie {
+    id :number;
+    original_title:string;
+    title:string;
+    overview:string;
+    vote_average:number;
+    runtime:number;
+    budget:number;
+    revenue:number;
+    backdrop_path:string
+
+}
