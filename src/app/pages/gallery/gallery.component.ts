35b7203ee13f76cbaeba67df335679cfa84c9d32
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-01-01 00:00:36 +0300

    feat: favoryList revised.

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/model/movie';
+import { MovieService } from 'src/app/services/movie.service';
+import {
+  BACKDROP_SIZE,
+  IMAGE_BASE_URL, POSTER_SIZE
+} from 'src/config';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  providers: [MovieService]
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movieService: MovieService,
+    private activatedRoute: ActivatedRoute) { }
+
+  @Input() movie: Movie = new Movie();
+  @Input() imageUrl: ""
+  @Input() backdropImageUrl: ""
+
+  movies: Array<Movie> = new Array<Movie>();
+  movieIds: Array<string> = new Array<string>();
+
+  imagePath: string="";
 
   ngOnInit(): void {
+    this.movieService.getMovieById("624860").subscribe(data => {
+      this.imagePath = data.poster_path
+    })
+
+    this.movieIds.push("624860");
+    this.movieIds.forEach(element => {
+      this.getMovies(element);
+    });
+    console.log(this.movies)
+    this.movies.map(movie=>{
+      this.imagePath=IMAGE_BASE_URL+BACKDROP_SIZE+movie.backdrop_path
+    })
+    console.log(IMAGE_BASE_URL)
+  }
+
+  getMovies(movieId: string) {
+    this.movieService.getMovieById(movieId).subscribe(data => {
+      this.movies.push(data)
+    })
   }
 
 }
