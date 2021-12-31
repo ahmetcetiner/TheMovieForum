1d984a9c21af3007efa75df8d40e32095dfd1bd1
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-31 17:21:47 +0300

    refactor: MovieService revised.

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
 import API from '../../API';
+import { HttpClient } from "@angular/common/http";
+import {
+  SEARCH_BASE_URL,
+  POPULAR_BASE_URL,
+  API_URL,
+  API_KEY,
+  REQUEST_TOKEN_URL,
+  LOGIN_URL,
+  SESSION_ID_URL
+} from 'src/config';
+import { Observable } from 'rxjs';
+import { Movie } from '../model/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+  
+  getMovieById(movieId:string):Observable<Movie>{
+    return this.httpClient.get<Movie>( API_URL+"movie/"+movieId+"?api_key="+API_KEY)
+   
+  }
 }
