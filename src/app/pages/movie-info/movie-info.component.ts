fd61408586b03b39686ab1a70235da297d04f415
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-02 02:21:36 +0300

    refactor: MovieInfoComponent revised.

--- src/app/pages/movie-info/movie-info.component.ts
+++ src/app/pages/movie-info/movie-info.component.ts
@@ -1,62 +1,65 @@
+import { Actor } from './../../model/actor';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/services/movie.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/model/movie';
-import {
-  IMAGE_BASE_URL, POSTER_SIZE
-} from 'src/config';
-import { Router } from '@angular/router';
+import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from 'src/config';
+import { Crew } from 'src/app/model/crew';
 
 @Component({
   selector: 'app-movie-info',
   templateUrl: './movie-info.component.html',
   styleUrls: ['./movie-info.component.scss'],
-  providers:[MovieService]
+  providers: [MovieService],
 })
 export class MovieInfoComponent implements OnInit {
+  constructor(
+    private movieService: MovieService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  constructor(private movieService:MovieService,
-    private activatedRoute:ActivatedRoute,private router: Router) { }
+  
+  imagePath: string;
+  imageUrl: string;
+  movie!: Movie;
+  backdropImageUrl: string;
 
-  @Input() movie : Movie  = new Movie();
-  @Input() imageUrl : ""
-  @Input() backdropImageUrl : ""
-
-
-  actorName : Array<string> = new Array<string>();
-  character :  Array<string>= new Array<string>();
-  actorImageUrl : Array<string>= new Array<string>();
-  imagePath :string = this.movie.poster_path ;
- 
-  movies : Movie;
-
-  sendModule : string 
-
- ngOnInit() {
-   this.sendModule="actor";
-   this.movieService.getMovieById(this.movie.id.toString()).subscribe(data=>{
-     this.imagePath=data.poster_path
-    this.movies = data
-   
-   })
-
-   console.log(this.imagePath)
+  @Input() set submittedMovie(movie: Movie) {
+    this.movie = movie;
+    this.imagePath =        IMAGE_BASE_URL + BACKDROP_SIZE + movie.poster_path;
+    this.imageUrl =         IMAGE_BASE_URL + BACKDROP_SIZE + movie.backdrop_path;
+    this.backdropImageUrl = IMAGE_BASE_URL + BACKDROP_SIZE + movie.poster_path;
   }
-  getModule (moduleName) {
-    switch(moduleName){
-      case "actor":
-        this.sendModule="actor"
+
+  actors : Actor[]
+  directors : Crew[]
+
+  sendModule: string = "actor";
+
+  ngOnInit() {
+     this.movieService.getActorsByMovieId(this.movie.id.toString()).subscribe((data) => {
+       this.actors=data.cast
+       this.directors= data.crew.filter(member => member.job=="Director");
+     })
+     
+  }
+
+  getModule(moduleName) {
+    switch (moduleName) {
+      case 'actor':
+        this.sendModule = 'actor';
         break;
-      case "reviews":
-        this.sendModule="reviews"
+      case 'reviews':
+        this.sendModule = 'reviews';
         break;
-      case "discussions":
-        this.sendModule="discussions"
-         break;
+      case 'discussions':
+        this.sendModule = 'discussions';
+        break;
       default:
-        this.sendModule="actor"
+        this.sendModule = 'actor';
         break;
     }
-};
+  }
 
+  get
 }
