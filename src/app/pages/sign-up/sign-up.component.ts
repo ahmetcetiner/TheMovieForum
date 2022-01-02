134da9a28a55120f14727192e7eb29a70041c13f
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-03 02:56:48 +0300

    refactor: SignUpComponent revised.

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { PopularMovie } from 'src/app/model/popularMovie';
+import { MovieService } from 'src/app/services/movie.service';
+import {
+  BACKDROP_SIZE,
+  IMAGE_BASE_URL
+} from 'src/config';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +14,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movieService:MovieService) { }
 
-  ngOnInit(): void {
+  movies : PopularMovie
+  imagePath:string;
+  resultIndex : number = Math.floor(Math.random() * 20);
+
+  ngOnInit(){
+    this.movieService.getBackdropImage().subscribe(data=>{
+      this.movies=  data;       
+      this.imagePath= IMAGE_BASE_URL+BACKDROP_SIZE+data.results[this.resultIndex].backdrop_path      
+    })   
   }
 
 }
