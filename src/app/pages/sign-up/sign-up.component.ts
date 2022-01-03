8e57f011d96303c9758109c755bae975cc492e89
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-03 18:02:16 +0300

    refactor: SignUpComponentl revised.

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,30 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { PopularMovie } from 'src/app/model/popularMovie';
 import { MovieService } from 'src/app/services/movie.service';
-import {
-  BACKDROP_SIZE,
-  IMAGE_BASE_URL
-} from 'src/config';
-
+import { BACKDROP_SIZE, IMAGE_BASE_URL } from 'src/config';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/model/users';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(private movieService: MovieService) {}
 
-  constructor(private movieService:MovieService) { }
+  model: User = new User();
+  imagePath: string;
+  resultIndex: number = Math.floor(Math.random() * 20);
 
-  movies : PopularMovie
-  imagePath:string;
-  resultIndex : number = Math.floor(Math.random() * 20);
-
-  ngOnInit(){
-    this.movieService.getBackdropImage().subscribe(data=>{
-      this.movies=  data;       
-      this.imagePath= IMAGE_BASE_URL+BACKDROP_SIZE+data.results[this.resultIndex].backdrop_path      
-    })   
+  ngOnInit() {
+    this.movieService.getBackdropImage().subscribe((data) => {
+      this.imagePath = IMAGE_BASE_URL + BACKDROP_SIZE + data.results[this.resultIndex].backdrop_path;
+    });
   }
-
+  signUp(form:NgForm){
+    alert(this.model.Password+this.model.UserName)
+  }
 }
