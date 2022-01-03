0677a20a4350b09049c3d6d4b3ff7d73472c4ecf
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-01-03 22:15:37 +0300

    refactor discussion revised

--- src/app/pages/discussions/discussions.component.ts
+++ src/app/pages/discussions/discussions.component.ts
@@ -11,9 +11,12 @@ export class DiscussionsComponent implements OnInit {
   constructor() { }
   discussions: Array<Discussion> = new Array<Discussion>();
   ngOnInit(): void {
+    this.getDiscussions()
+    console.log(this.discussions)
   }
 
   getDiscussions(){
+    this.discussions.push(new Discussion("https://ui-avatars.com/api/?size=128","ahmetcetinerr","02/01/2022","Natashanin kendini feda etmesi.",250))
     this.discussions.push(new Discussion("https://ui-avatars.com/api/?size=128","ahmetcetinerr","02/01/2022","Natashanin kendini feda etmesi.",250))
   }
 }
