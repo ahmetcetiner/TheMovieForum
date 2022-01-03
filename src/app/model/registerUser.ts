118a6cbac2c69e7934801eda415ad65ced2c2461
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-03 22:14:50 +0300

    feat: RegisterUser added.

---
+++ src/app/model/registerUser.ts
@@ -0,0 +1,9 @@
+export class RegisterUser {
+    FirstName !: string;
+    LastName !: string;
+    Email !: string;    
+    UserName!: string;
+    Password!: string;
+    AvatarUrl !: string;
+
+}
