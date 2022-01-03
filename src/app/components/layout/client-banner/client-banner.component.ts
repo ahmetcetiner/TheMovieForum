67aed72a647792611aa6c6f5836f9fa78514ebb9
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-04 01:51:48 +0300

    refactor: ClientBannerComponent revised.

--- src/app/components/layout/client-banner/client-banner.component.ts
+++ src/app/components/layout/client-banner/client-banner.component.ts
@@ -1,15 +1,26 @@
+import { UserService } from './../../../services/user.service';
+import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client-banner',
   templateUrl: './client-banner.component.html',
-  styleUrls: ['./client-banner.component.scss']
+  styleUrls: ['./client-banner.component.scss'],
 })
 export class ClientBannerComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    console.log(this.authService.loggedIn());
   }
 
+  get isAuthenticated() {
+    return this.authService.loggedIn();
+  }
+  logOut() {
+    this.authService.logOut();
+  }
 }
