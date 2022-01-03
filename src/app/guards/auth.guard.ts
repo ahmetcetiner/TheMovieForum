3f157cd43ff10958f2b73d7003d4e98085f5f255
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-04 01:53:21 +0300

    feat: AuthGuard added.

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    let logged = this.authService.loggedIn();
+    if(logged){
+      this.router.navigate(["/"]);
+      return false;
+    }  
+    return true;
+    
+  }
+
+}
