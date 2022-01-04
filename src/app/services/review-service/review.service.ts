513b979efdb151db6eaa3f5a953c5cb37479caf0
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-05 01:48:31 +0300

    refactor: ReviewService revised.

--- src/app/services/review-service/review.service.ts
+++ src/app/services/review-service/review.service.ts
@@ -6,12 +6,10 @@ import { Review } from 'src/app/model/review';
 import { HEROKU_API_URL } from 'src/config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReviewService {
-
-constructor(private httpClient : HttpClient) { }
-
+  constructor(private httpClient: HttpClient) {}
 
   /*getReviewById(id: number) {
     let headers = new HttpHeaders();
@@ -23,8 +21,42 @@ constructor(private httpClient : HttpClient) { }
     headers = headers.append('token', tokenGetter());
 
     params = params.append('Id',id.toString())
-    return this.httpClient.get<User>(
+    return this.httpClient.get<Review>(
       HEROKU_API_URL + 'review',
+      {headers:headers}
+                 
+    );
+  }*/
+
+  /*getReviewByMovieId(id: number) {
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
+
+    const options ={ params: new HttpParams().set('Id', "8")} ;
+ 
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('token', tokenGetter());
+
+    params = params.append('Id',id.toString())
+    return this.httpClient.get<Review>(
+      HEROKU_API_URL + 'movieReviewAll',
+      {headers:headers}
+                 
+    );
+  }*/
+
+  /*getReviewByUserId(id: number) {
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
+
+    const options ={ params: new HttpParams().set('Id', "8")} ;
+ 
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('token', tokenGetter());
+
+    params = params.append('Id',id.toString())
+    return this.httpClient.get<Review>(
+      HEROKU_API_URL + 'reviews',
       {headers:headers}
                  
     );
@@ -40,25 +72,45 @@ constructor(private httpClient : HttpClient) { }
     });
   }
 
-  updateReview(review: Review){
+  updateReview(review: Review) {
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
     headers = headers.append('token', tokenGetter());
-    return this.httpClient.put<Review>(HEROKU_API_URL + 'review',review, {headers: headers});
+    return this.httpClient.put<Review>(HEROKU_API_URL + 'review', review, {
+      headers: headers,
+    });
   }
 
- /* deletReview(review: Review){
+  addReview(review: Review) {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('token', tokenGetter());
+
+    return this.httpClient.put<Review>(
+      HEROKU_API_URL + 'review',
+      {
+        UserId: review.UserId,
+        Title: review.Title,
+        MovieId: review.MovieId,
+        MessageText: review.MessageText,
+        ReviewLike: review.ReviewLike,
+        ReviewDislike:review.ReviewDislike,
+        CreatedDate:review.CreatedDate
+      },
+      { headers: headers }
+    );
+  }
+
+  /* deletReview(review: Review){
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
     headers = headers.append('token', tokenGetter());
     return this.httpClient.delete<Review>(HEROKU_API_URL + 'review',user, {headers: headers});
   }*/
 
-
   getToken() {
     let token = tokenGetter();
     if (token !== null) return true;
     return false;
   }
-
 }
