d092279d7ed7d7ad8bf6569b5402961847db33f2
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-05 01:39:59 +0300

    feat: Review data model added.

---
+++ src/app/model/review.ts
@@ -0,0 +1,10 @@
+export class Review {
+  id : number;
+  UserId : number;
+  Title: string;
+  MovieId: number;
+  MessageText: string;
+  ReviewLike: number;
+  ReviewDislike: number;
+  CreatedDate: Date;
+}
