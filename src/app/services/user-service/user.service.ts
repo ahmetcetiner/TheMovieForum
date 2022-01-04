822f8f924eecc548de21fb38a1c2b0382672c4bd
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-05 01:29:50 +0300

    refactor: UserService revised.

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/model/users';
 import { HEROKU_API_URL } from 'src/config';
 import { tokenGetter } from '../../app.module';
 
@@ -9,13 +11,47 @@ import { tokenGetter } from '../../app.module';
 export class UserService {
   constructor(private httpClient: HttpClient) {}
 
-  getUserById(id: number) {
+  /*getUserById(id: number) {
+    let headers = new HttpHeaders();
+    let params = new HttpParams();
+
+    const options ={ params: new HttpParams().set('Id', "8")} ;
+ 
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('token', tokenGetter());
+
+    params = params.append('Id',id.toString())
+    return this.httpClient.get<User>(
+      HEROKU_API_URL + 'user',
+      {headers:headers}
+                 
+    );
+  }*/
+
+  getUsers(): Observable<User[]> {
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
     headers = headers.append('token', tokenGetter());
-    
-    return this.httpClient.post(HEROKU_API_URL + 'login', id, {headers: headers});      
+
+    return this.httpClient.get<User[]>(HEROKU_API_URL + 'userAll', {
+      headers: headers,
+    });
   }
+
+  updateUser(user: User){
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('token', tokenGetter());
+    return this.httpClient.put<User>(HEROKU_API_URL + 'user',user, {headers: headers});
+  }
+
+ /* deleteUser(user: User){
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('token', tokenGetter());
+    return this.httpClient.delete<User>(HEROKU_API_URL + 'user',user, {headers: headers});
+  }*/
+
 
   getToken() {
     let token = tokenGetter();
