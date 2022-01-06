8fefba2082361189df8f39e1ac96e8adc24d2245
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-07 02:35:24 +0300

    refactor: CommentComponent revised.

--- src/app/pages/comment/comment.component.ts
+++ src/app/pages/comment/comment.component.ts
@@ -1,4 +1,6 @@
+import {  DatePipe, formatDate } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { idGetter } from 'src/app/app.module';
 import { Review } from 'src/app/model/review';
 import { MovieService } from 'src/app/services/movie-service/movie.service';
 import { ReviewService } from 'src/app/services/review-service/review.service';
@@ -16,25 +18,44 @@ export class CommentComponent implements OnInit {
     private reviewService: ReviewService
     ) { }
 
+    datePipe :  DatePipe = new DatePipe('en-US');
+
   myMovieId: number;
   reviewInput: boolean;
-  reviewText: Text;
+  reviewText: string;
   reviewTitle: string;
 
+  model : Review = new Review
   @Input() set movieId(id) {
     this.myMovieId = id;
   }
   
   ngOnInit(): void {
-
     this.getReviewArea(this.reviewInput)
   }
 
   getReviewArea(reviewInput: boolean) {
     this.reviewInput = reviewInput;
   }
+
+  setModel(){
+    this.model.UserId=parseInt(idGetter())
+    this.model.MovieId=this.myMovieId;
+    this.model.ReviewLike=0
+    this.model.ReviewDislike=0
+    this.model.MessageText=this.reviewText
+    this.model.Title=this.reviewTitle    
+   
+    let d = new Date()
+    let date= this.datePipe.transform(new Date(),'yyyy-MM-dd')
+    this.model.CreatedDate= date
+    console.log()
+
+  }
   reviewAdd() {
-   this.reviewService.addReview()
+    this.setModel()
+    console.log(this.model)
+    this.reviewService.addReview(this.model).subscribe(data=>{})
   }
 
 }
