6b523d571801eac58fa058495e58c8d95ee6a858
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-07 11:37:47 +0300

    refactor: ProfilePageComponent revised.

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,7 @@
+import { User } from './../../model/users';
+import { UserService } from './../../services/user-service/user.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute,
+    private userService:UserService) { }
+
+    user : User[]
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getUser(params['userId']);
+    });
+  }
+
+  getUser(userId){
+    this.userService.getUserById(userId).subscribe(data=>{
+      this.user=data
+    })
   }
 
 }
