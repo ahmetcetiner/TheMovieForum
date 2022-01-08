baaa442207cbac92fb8c6c46ef9db677e843cbc8
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-09 01:19:05 +0300

    refactor : TrailerComponent revised.

--- src/app/pages/trailer/trailer.component.ts
+++ src/app/pages/trailer/trailer.component.ts
@@ -1,25 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { YouTubePlayer, YouTubePlayerModule } from '@angular/youtube-player';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MovieService } from 'src/app/services/movie-service/movie.service';
+import { YtPlayerService } from 'yt-player-angular';
+import { YouTubePlayer, YouTubePlayerModule } from '@angular/youtube-player';
 
 @Component({
   selector: 'app-trailer',
   templateUrl: './trailer.component.html',
-  styleUrls: ['./trailer.component.scss']
+  styleUrls: ['./trailer.component.scss'],
 })
 export class TrailerComponent implements OnInit {
 
-  constructor(private movieService: MovieService) { }
+  constructor(
+    private movieService: MovieService,
+  ) {}
   _videoId: string;
+
   @Input() set videoId(videoId: string) {
     this._videoId = videoId;
+  }  
+  ngOnInit() { 
+  
+  }
+ player : YouTubePlayer
+  onReady() {
+    this.player.mute();         
+    this.player.playVideo();    
   }
 
-
-  player: YT.Player
-  ngOnInit(): void {
-    console.log(this._videoId)
+  // Loop
+  onStateChange(event) {
+    if (event.data === 0) {
+      this.player.playVideo();  
+    }
   }
-
-
+ 
+  ngOnDestroy(){
+    this.player.ngOnDestroy()
+  }
+  deneme() {
+   // this.player.pauseVideo();
+  }
 }
