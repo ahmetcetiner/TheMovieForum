31e39f7c10551f822a31a759399515c4850c6258
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-08 19:37:42 +0300

    refactor: Review revised.

--- src/app/model/review.ts
+++ src/app/model/review.ts
@@ -1,13 +1,12 @@
-import { User } from 'src/app/model/users';
 export class Review {
   Id : number;
   UserId : number;
   Title: string;
   MovieId: number;
   MessageText: string;
-  ReviewLike: number;
-  ReviewDislike: number;
   CreatedDate: string;
-
-  User : User
+  FirstName : string;
+  LastName : string;  
+  UserName!: string;
+  AvatarUrl !: string;
 }
