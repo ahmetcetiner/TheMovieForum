a3fada3881f512a7c5b8eddbf3befa2d08d18842
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-01-10 00:12:43 +0300

    refactor: search companent revised

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/model/movie';
+import { MovieService } from 'src/app/services/movie-service/movie.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movieService: MovieService,
+    private activatedRoute: ActivatedRoute,) { }
 
+  movies: Array<Movie> = new Array<Movie>()
+  pageValue: number = 1
+  serchText: string
+  searchPage: number
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getMovie(params['serchText'], this.pageValue);
+      this.serchText = params['serchText']
+      this.pageValue=1
+    });
+
   }
 
+  getMovie(serchText: string, pageValue: number) {
+    this.movieService.searchMovies(serchText, pageValue).subscribe(data => {
+      this.movies = data.results
+      this.searchPage = data.total_pages
+    })
+  }
+
+  getNewPage() {
+    if (this.pageValue < this.searchPage) {
+      this.pageValue += 1
+      this.getMovie(this.serchText, this.pageValue)
+    }
+  }
+  getOldPage() {
+    if (this.pageValue > 1) {
+    this.pageValue -= 1
+    this.getMovie(this.serchText, this.pageValue)
+    }
+  }
 }
