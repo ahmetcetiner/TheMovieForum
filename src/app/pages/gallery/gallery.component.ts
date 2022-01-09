4d86f0554207085f1291be38c8aab11f804273d8
Ahmet ÇETİNER <ahmetcetinerrrymuh@gmail.com>
2022-01-09 19:07:05 +0300

    refactor: gallery companet revised

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -18,34 +18,14 @@ export class GalleryComponent implements OnInit {
 
   constructor(private movieService: MovieService,
     private activatedRoute: ActivatedRoute) { }
+    @Input() movies: Array<Movie>
 
-  @Input() movie: Movie = new Movie();
-  @Input() imageUrl: ""
-  @Input() backdropImageUrl: ""
-
-  movies: Array<List> = new Array<List>();
-  movieIds: Array<string> = new Array<string>();
-
-  imagePath:Array<string> = new Array<string>();
 
   ngOnInit(): void {
-    this.movieIds.push("624860");
-    this.movieIds.push("634649");
-    this.movieIds.push("102382");
-    this.movieIds.push("831223");
-    this.movieIds.push("585245");
-    
-    this.movieIds.forEach(element => {
-      this.getMovies(element);
-    });  
-    console.log(this.movies)
+
   }
 
-
-  getMovies(movieId: string) {
-    this.movieService.getMovieById(movieId).subscribe(data => {
-     // this.imagePath.push(IMAGE_BASE_URL+BACKDROP_SIZE+data.poster_path)
-     // this.movies.push(new List(IMAGE_BASE_URL+BACKDROP_SIZE+data.poster_path,data.title,data.vote_average,data.id))
-    })
+  getdeneme(){
+    console.log(this.movies)
   }
 }
