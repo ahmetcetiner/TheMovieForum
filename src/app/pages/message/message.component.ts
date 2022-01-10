0eebf74ebe68c5b9d2a7b1c88679cc5b03c26a9c
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2022-01-10 20:56:15 +0300

    refactor: MessageComponent revised.

--- src/app/pages/message/message.component.ts
+++ src/app/pages/message/message.component.ts
@@ -1,4 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { idGetter } from 'src/app/app.module';
 import { Message } from 'src/app/model/message';
 import { MessageService } from 'src/app/services/message-service/message.service';
 
@@ -9,16 +13,69 @@ import { MessageService } from 'src/app/services/message-service/message.service
 })
 export class MessageComponent implements OnInit {
 
-  constructor(private messageService : MessageService) { }
+  constructor(private messageService : MessageService,
+    private formBuilder: FormBuilder,
+      private activatedRoute: ActivatedRoute,) { }
+
   @Input() messasges: Array<Message>
 
-  _discussionIds="5"
+  datePipe: DatePipe = new DatePipe('en-US');
+
+  
+  movieId: number;
+  discussionId : number
+  messageInput: boolean;
+  messageText: string;
+  messageTitle: string;
+
+  messageForm: FormGroup;
+  message: Message = new Message();
+
+  model: Message = new Message();
 
   ngOnInit() {
-    this.getMessages()
+    this.createMessageForm();
+    this.activatedRoute.params.subscribe((params) => {   
+      this.movieId=params['movieId']
+      this.discussionId=params['discussionId']
+    });
   }
 
-  getMessages() {
-    
+  createMessageForm() {
+    this.messageForm = this.formBuilder.group({      
+      Text: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(500),
+        ],
+      ],
+    });
   }
+
+  getMessageArea(messageInput: boolean) {
+    this.messageInput = messageInput;
+  }
+
+  setModel() {
+    this.message.UserId = Number(idGetter());
+    this.message.DiscussionId = this.discussionId;
+    this.message.MessageText = this.messageForm.controls['Text'].value;
+   
+
+    let date = this.datePipe.transform(new Date(), 'yyyy-MM-dd HH:mm');
+    this.message.CreatedDate = date;
+  }
+
+  sengMessage() {
+    if (this.messageForm.valid) {
+      this.setModel();
+      this.messageService.sendMessage(this.message);
+    }
+    setTimeout(() => {
+      window.location.reload()
+    }, 200)
+  }
+
 }
